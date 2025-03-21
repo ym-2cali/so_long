d320749bafab494e24853959c9dc926f05c003e6
Yahya El Maachi <yael-maa@e2r9p15.1337.ma>
2025-03-21 11:51:14 +0000

    enemy patrol done

--- bonus/src_bonus/enemy/enemy_move_algo.c
+++ bonus/src_bonus/enemy/enemy_move_algo.c
@@ -6,7 +6,7 @@
 /*   By: yael-maa <yael-maa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 03:08:54 by yael-maa          #+#    #+#             */
-/*   Updated: 2025/03/21 05:51:15 by yael-maa         ###   ########.fr       */
+/*   Updated: 2025/03/21 11:48:28 by yael-maa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,22 +41,22 @@ void	enemy_direction_helper(t_map *map, int x, int y)
 				move_enemy_right(map);
 		}
 	}
-	else if (y == 0)
-	{
-		if ((x < 0 && map->arr[map->enemy_pos.y][map->enemy_pos.x + 1] != '1' && map->arr[map->enemy_pos.y][map->enemy_pos.x + 1] != 'E')
-		|| (x > 0 && map->arr[map->enemy_pos.y][map->enemy_pos.x - 1] == '1' && map->arr[map->enemy_pos.y][map->enemy_pos.x - 1] == 'E'))
-			move_enemy_right(map);
-		else if ((x > 0 && map->arr[map->enemy_pos.y][map->enemy_pos.x - 1] != '1' && map->arr[map->enemy_pos.y][map->enemy_pos.x - 1] != 'E')
-		|| (x < 0 && map->arr[map->enemy_pos.y][map->enemy_pos.x + 1] == '1' && map->arr[map->enemy_pos.y][map->enemy_pos.x + 1] == 'E'))
-			move_enemy_left(map);
-		else
-		{
-			if (map->arr[map->enemy_pos.y][map->enemy_pos.x - 1] != '1' && map->arr[map->enemy_pos.y][map->enemy_pos.x - 1] != 'E')
-				move_enemy_left(map);
-			else if (map->arr[map->enemy_pos.y][map->enemy_pos.x + 1] != '1' && map->arr[map->enemy_pos.y][map->enemy_pos.x + 1] != 'E')
-				move_enemy_right(map);
-		}
-	}
+	// else if (y == 0)
+	// {
+	// 	if ((x < 0 && map->arr[map->enemy_pos.y][map->enemy_pos.x + 1] != '1' && map->arr[map->enemy_pos.y][map->enemy_pos.x + 1] != 'E')
+	// 	|| (x > 0 && map->arr[map->enemy_pos.y][map->enemy_pos.x - 1] == '1' && map->arr[map->enemy_pos.y][map->enemy_pos.x - 1] == 'E'))
+	// 		move_enemy_right(map);
+	// 	else if ((x > 0 && map->arr[map->enemy_pos.y][map->enemy_pos.x - 1] != '1' && map->arr[map->enemy_pos.y][map->enemy_pos.x - 1] != 'E')
+	// 	|| (x < 0 && map->arr[map->enemy_pos.y][map->enemy_pos.x + 1] == '1' && map->arr[map->enemy_pos.y][map->enemy_pos.x + 1] == 'E'))
+	// 		move_enemy_left(map);
+	// 	else
+	// 	{
+	// 		if (map->arr[map->enemy_pos.y][map->enemy_pos.x - 1] != '1' && map->arr[map->enemy_pos.y][map->enemy_pos.x - 1] != 'E')
+	// 			move_enemy_left(map);
+	// 		else if (map->arr[map->enemy_pos.y][map->enemy_pos.x + 1] != '1' && map->arr[map->enemy_pos.y][map->enemy_pos.x + 1] != 'E')
+	// 			move_enemy_right(map);
+	// 	}
+	// }
 }
 void	enemy_direction(t_map *map)
 {
@@ -253,22 +253,23 @@ void	enemy_direction(t_map *map)
 	if (time_dif.tv_sec >= (1000000 / 2))
 	{
 		if (((map->arr[map->enemy_pos.y - 1][map->enemy_pos.x] != '1' && map->arr[map->enemy_pos.y - 1][map->enemy_pos.x] != 'E') && (((x >= 0) && (y >= x)) || ((x <= 0) && (-x) <= y)))
-			|| ((map->arr[map->enemy_pos.y][map->enemy_pos.x - 1] == '1' || map->arr[map->enemy_pos.y][map->enemy_pos.x - 1] == 'E') && (((x >= y) && (y >= 0)) || ((y <= 0) && x >= (-y))))
+			|| ((map->arr[map->enemy_pos.y][map->enemy_pos.x - 1] == '1' || map->arr[map->enemy_pos.y][map->enemy_pos.x - 1] == 'E') && (((x >= y) && (y >= 0)) || ((y <= 0) && x > (-y))))
 			|| ((map->arr[map->enemy_pos.y][map->enemy_pos.x + 1] == '1' || map->arr[map->enemy_pos.y][map->enemy_pos.x + 1] == 'E') && ((((-x) >= y) && (y >= 0)) || ((y <= 0) && x <= y))))
 			move_enemy_up(map);
-		else if (((map->arr[map->enemy_pos.y][map->enemy_pos.x + 1] != '1' && map->arr[map->enemy_pos.y][map->enemy_pos.x + 1] != 'E') && ((((-x) > y) && (y >= 0)) || ((y <= 0) && x < y) || ((x == y && y < 0) && !((x == y) && y > 0))))
-			|| ((map->arr[map->enemy_pos.y - 1][map->enemy_pos.x] == '1' || map->arr[map->enemy_pos.y - 1][map->enemy_pos.x] == 'E') && (((x >= 0) && (y > x)) || ((x <= 0) && (-x) < y)))
-			|| ((map->arr[map->enemy_pos.y + 1][map->enemy_pos.x] == '1' || map->arr[map->enemy_pos.y + 1][map->enemy_pos.x] == 'E') && (((x >= 0) && ((-y) > x)) || ((x <= 0) && x < y))))
+		else if (((map->arr[map->enemy_pos.y][map->enemy_pos.x + 1] != '1' && map->arr[map->enemy_pos.y][map->enemy_pos.x + 1] != 'E') && ((((-x) >= y) && (y >= 0)) || ((y <= 0) && x < y) || ((x == y && y < 0) && !((x == y) && y > 0))))
+			// || ((map->arr[map->enemy_pos.y - 1][map->enemy_pos.x] == '1' || map->arr[map->enemy_pos.y - 1][map->enemy_pos.x] == 'E') && (((x >= 0) && (y > x)) || ((x <= 0) && (-x) <= y)))
+			// || ((map->arr[map->enemy_pos.y + 1][map->enemy_pos.x] == '1' || map->arr[map->enemy_pos.y + 1][map->enemy_pos.x] == 'E' || map->arr[map->enemy_pos.y][map->enemy_pos.x + 1] == '1') && (((x >= 0) && (((-y) >= x))))))
+			|| ((map->arr[map->enemy_pos.y + 1][map->enemy_pos.x] == '1' || map->arr[map->enemy_pos.y + 1][map->enemy_pos.x] == 'E' ) && ((y < 0 && x <= 0 && (-x) <= (-y)) || (y == 0 && x < 0)) ))
 			move_enemy_right(map);
-		else if (((map->arr[map->enemy_pos.y + 1][map->enemy_pos.x] != '1' && map->arr[map->enemy_pos.y + 1][map->enemy_pos.x] != 'E') && (((x >= 0) && ((-y) >= x)) || ((x <= 0) && (-x) <= y)))
+		else if (((map->arr[map->enemy_pos.y + 1][map->enemy_pos.x] != '1' && map->arr[map->enemy_pos.y + 1][map->enemy_pos.x] != 'E') && (((x >= 0) && ((-y) >= x)) || ((x <= 0) && x > y)))
 			|| ((map->arr[map->enemy_pos.y][map->enemy_pos.x - 1] == '1' || map->arr[map->enemy_pos.y][map->enemy_pos.x - 1] == 'E') && (((x >= y) && (y >= 0)) || ((y <= 0) && x >= (-y))))
 			|| ((map->arr[map->enemy_pos.y][map->enemy_pos.x + 1] == '1' || map->arr[map->enemy_pos.y][map->enemy_pos.x + 1] == 'E') && ((((-x) >= y) && (y >= 0)) || ((y <= 0) && x <= y))))
 			move_enemy_down(map);
-		else if (((map->arr[map->enemy_pos.y][map->enemy_pos.x - 1] != '1' && map->arr[map->enemy_pos.y][map->enemy_pos.x - 1] != 'E') && (((x > y) && (y >= 0)) || ((y <= 0) && x > (-y)) || ((x == y) && y > 0)))
-			|| ((map->arr[map->enemy_pos.y - 1][map->enemy_pos.x] == '1' || map->arr[map->enemy_pos.y - 1][map->enemy_pos.x] == 'E') && (((x >= 0) && (y > x)) || ((x <= 0) && (-x) < y)))
-			|| ((map->arr[map->enemy_pos.y + 1][map->enemy_pos.x] == '1' || map->arr[map->enemy_pos.y + 1][map->enemy_pos.x] == 'E') && (((x >= 0) && ((-y) > x)) || ((x <= 0) && x < y))))
+		else if (((map->arr[map->enemy_pos.y][map->enemy_pos.x - 1] != '1' && map->arr[map->enemy_pos.y][map->enemy_pos.x - 1] != 'E') && (((x >= y) && (y >= 0)) || ((y <= 0) && x > (-y)) || ((x == y) && y > 0)))
+			|| ((map->arr[map->enemy_pos.y - 1][map->enemy_pos.x] == '1' || map->arr[map->enemy_pos.y - 1][map->enemy_pos.x] == 'E') && (((x >= 0) && (y > x)) || ((x <= 0) && (-x) <= y)))
+			|| ((map->arr[map->enemy_pos.y + 1][map->enemy_pos.x] == '1' || map->arr[map->enemy_pos.y + 1][map->enemy_pos.x] == 'E') && (((x > 0) && ((-y) >= x)) || ((x <= 0) && x <= y))))
 			move_enemy_left(map);
-		enemy_direction_helper(map, x, y);
+		// enemy_direction_helper(map, x, y);
 		map->last_move_time = current_time;
 	}
 	// }
